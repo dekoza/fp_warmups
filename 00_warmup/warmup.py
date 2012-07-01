4944fd5f6193e15cdb4996050c5d9af42f033170
Dominik Kozaczko <dominik@kozaczko.info>
2012-07-01 17:49:36 +0200

    more descriptive option names in settings/args

--- 00_warmup/warmup.py
+++ 00_warmup/warmup.py
@@ -44,8 +44,17 @@ Zabezpieczcie się przed:
 Sposób raportowania i wyświetlania jest dowolny, byle był zrozumiały ;-).
 </cytat>
 
+
+NIEZGODNOŚCI ZE SPECYFIKACJĄ:
+- Ad. 2: delimiterem jest każdy znak nie będący znakiem alfanumerycznym lub podkreślnikiem (czyli delimiterem są np. białe znaki, kropka, przecinek)
+  Aby to zmienić należy zmodyfikować wyrażenie regularne w funkcji get_words(). Wolę moją wersję, bo zwraca więcej wyrazów :)
+  Patrz też: ZNANE BŁĘDY (poniżej)
+
+
 ZNANE BŁĘDY:
-- Wyrazy nie mogą zawierać polskich liter.
+- Wyrazy nie mogą zawierać polskich liter (w pkt. 3 specyfikacji jest wyraźnie napisane, że używamy tylko alfabetu łacińskiego, ale dla własnej satysfakcji chcę to rozszerzyć)
+  Niestety póki co polskie znaki są traktowane jako delimitery i nie pomaga użycie flag UNICODE/LOCALE
+
 
 Jak testować z użyciem unittest:
 $ cd /ścieżka/do/katalogu/z/plikiem
@@ -57,6 +66,13 @@ TODO:
 """
 __author__ = 'Dominik Kozaczko'
 
+# domyślna konfiguracja
+
+SETTINGS = {
+    'MIN_WORD_LENGTH': 4,        # minimalna długość słowa
+    'MIN_WORDS_PICKED': 4,          # minimalna liczba słów
+}
+
 # importy
 
 import os
@@ -65,12 +81,6 @@ import re
 import random
 import unittest
 import argparse
-
-# domyślna konfiguracja
-SETTINGS = {
-    'MIN_LENGTH': 4,        # minimalna długość słowa
-    'HOW_MANY': 4,          # minimalna liczba słów
-}
 
 # Definicje wyjątków:
 
@@ -101,12 +111,12 @@ def parse_arguments():
     Zmienia domyślną konfigurację (na czas wykonania) w zależności od podanych parametrów."""
     parser = argparse.ArgumentParser(description="Randomly pick some words from a file and display common letters")
     parser.add_argument('file', metavar='FILE', help="File to play with; if ommited, you will be asked for a path", nargs='?', default=None)
-    parser.add_argument('-l', '--min-length', metavar='L', type=int, help="Minimum length of a word (default: {})".format(SETTINGS['MIN_LENGTH']), default=SETTINGS['MIN_LENGTH'])
-    parser.add_argument('-w', '--how-many', metavar='N', type=int, help="How many words should be picked (default: {})".format(SETTINGS['HOW_MANY']), default=SETTINGS['HOW_MANY'])
+    parser.add_argument('-l', '--min-word-length', metavar='L', type=int, help="Minimum length of a word (default: {})".format(SETTINGS['MIN_WORD_LENGTH']), default=SETTINGS['MIN_WORD_LENGTH'])
+    parser.add_argument('-w', '--min-words-picked', metavar='N', type=int, help="How many words should be picked (default: {})".format(SETTINGS['MIN_WORDS_PICKED']), default=SETTINGS['MIN_WORDS_PICKED'])
     args = parser.parse_args()
     # nie jestem pewny, czy to w dobrym guście, ale pasuje do Zen :)
-    SETTINGS['MIN_LENGTH'] = args.min_length
-    SETTINGS['HOW_MANY'] = args.how_many
+    SETTINGS['MIN_WORD_LENGTH'] = args.min_word_length
+    SETTINGS['MIN_WORDS_PICKED'] = args.min_words_picked
     return args
 
 def get_path():
@@ -138,35 +148,39 @@ def get_file_content(path):
     else:
         raise FilePathError, "Nieprawidłowa ścieżka"
 
-def get_words(words, min_length=None, how_many=None):
+def get_words(words, min_word_length=None, min_words_picked=None):
     """Usuwa słowa krótsze niż czteroliterowe ORAZ "wyrazy" zawierające niepoprawne znaki.
     Jako argument przyjmuje łańcuch i zwraca listę z oczekiwanymi wyrazami."""
-    # wykorzystuję wyrażenia regularne - lepiej się nie da :) przy okazji wykorzystuję locals(), by łatwo dostać się do potrzebnej zmiennej
-    if how_many is None:
-        how_many = SETTINGS.get('HOW_MANY')
-    if min_length is None:
-        min_length = SETTINGS.get('MIN_LENGTH')
-    pattern = re.compile(r"\b[a-zA-Z]{%(min_length)d,}\b" % locals())
+    if min_words_picked is None:
+        min_words_picked = SETTINGS.get('MIN_WORDS_PICKED')
+    if min_word_length is None:
+        min_word_length = SETTINGS.get('MIN_WORD_LENGTH')
+
+    # wykorzystuję wyrażenia regularne
+    # przy okazji wykorzystuję locals(), by łatwo dostać się do potrzebnej zmiennej
+    # ostatni parametr TEORETYCZNIE wyklucza polskie znaki z grupy \b
+    pattern = re.compile(r"\b[a-zA-Z]{%(min_word_length)d,}\b" % locals(), re.U|re.L)
     result = re.findall(pattern, words)
-    if len(result) >= how_many:
+    if len(result) >= min_words_picked:
         return result
     else:
-        raise NotEnoughCorrectWordsError, "Plik zawiera za mało poprawnych wyrazów (potrzeba co najmniej %(needed)d, otrzymano %(got)d)" % {'needed':how_many, 'got': len(result)}
+        raise NotEnoughCorrectWordsError, "Plik zawiera za mało poprawnych wyrazów (potrzeba co najmniej %(needed)d, otrzymano %(got)d)" % {'needed':min_words_picked, 'got': len(result)}
 
-def get_random_words(wordlist, how_many=None):
+def get_random_words(wordlist, min_words_picked=None):
     """Losuje kilka elementów z listy i zwraca jako listę."""
-    if how_many is None:
-        how_many = SETTINGS.get('HOW_MANY')
-    if len(wordlist)>=how_many:
-        return random.sample(wordlist,how_many)
+    if min_words_picked is None:
+        min_words_picked = SETTINGS.get('MIN_WORDS_PICKED')
+    if len(wordlist)>=min_words_picked:
+        return random.sample(wordlist,min_words_picked)
     else:
-        raise NotEnoughWordsError, "Za mało wyrazów. Potrzeba co najmniej %(needed)d, otrzymano %(got)d." % {'needed': how_many, 'got': len(wordlist)}
+        raise NotEnoughWordsError, "Za mało wyrazów. Potrzeba co najmniej %(needed)d, otrzymano %(got)d." % {'needed': min_words_picked, 'got': len(wordlist)}
         # ten wyjątek normalnie nie powinien nigdy zostać zgłoszony w trakcie działania programu
         # powód jest prosty: zbyt mała liczba wyrazów zostanie wcześniej wyłapana w funkcji get_words :)
 
 def find_intersections(wordlist):
     """Znajduje część wspólną w wyrazach podanych poprzez listę i zwraca jako listę."""
-    data = [set(word.lower()) for word in wordlist] # korzystam z listy składanej - zamieniam każdy wyraz na małe litery, przerabiam na zbiór i każdy ze zbiorów wrzucam jako element listy
+    # korzystam z listy składanej - zamieniam każdy wyraz na małe litery, przerabiam na zbiór i każdy ze zbiorów wrzucam jako element listy
+    data = [set(word.lower()) for word in wordlist]
     return list(set.intersection(*data)) # zwraca listę elementów należących do części wspólnej zbiorów rozpakowanych z listy :)
 
 def do_your_business():
@@ -184,7 +198,7 @@ def do_your_business():
         for letter in result:
             print(letter)
     else:
-        print "Wyrazy nie mają części wspólnej"
+        print "*** Wyrazy nie mają części wspólnej"
 
 def main():
     try:
@@ -203,25 +217,25 @@ class TestWarmup(unittest.TestCase):
 
         test_input = "1 ala Ala ma kot h1"
         expected_result = []
-        self.assertListEqual(get_words(test_input, min_length=4, how_many=0), expected_result)
+        self.assertListEqual(get_words(test_input, min_word_length=4, min_words_picked=0), expected_result)
 
         test_input = "NIkto 1 ala Aleksandra ma kota h1123123 234234"
         expected_result = ['NIkto', 'Aleksandra', 'kota']
-        self.assertListEqual(get_words(test_input, min_length=4, how_many=0), expected_result)
+        self.assertListEqual(get_words(test_input, min_word_length=4, min_words_picked=0), expected_result)
 
         test_input = "1 ala Aleksandra ma kota h1"
-        self.assertRaises(NotEnoughCorrectWordsError, get_words, words=test_input,min_length=4, how_many=4)
+        self.assertRaises(NotEnoughCorrectWordsError, get_words, words=test_input,min_word_length=4, min_words_picked=4)
 
     def test_get_random_words(self):
         self.assertRaises(TypeError, get_random_words)
 
         test_input = ["Ala", 'ma', 'fajnego', 'kota']
-        self.assertListEqual(sorted(get_random_words(test_input, how_many=4)), sorted(test_input))
+        self.assertListEqual(sorted(get_random_words(test_input, min_words_picked=4)), sorted(test_input))
         # sortowanie obu list jest konieczne ze względu na losową kolejność elementów w wyniku
         # zamiast tego można by ewentualnie zamienić listy na zbiory i porównać
 
         test_input = ["Ala", 'ma', 'kota']
-        self.assertRaises(NotEnoughWordsError, get_random_words, wordlist=test_input, how_many=4)
+        self.assertRaises(NotEnoughWordsError, get_random_words, wordlist=test_input, min_words_picked=4)
 
     def test_find_intersections(self):
         self.assertRaises(TypeError, find_intersections)
