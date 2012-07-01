c88e92ad183a548c215983ecdcb6b4002a5d59ed
Dominik Kozaczko <dominik@kozaczko.info>
2012-07-01 10:00:44 +0200

    tabs -> spaces

--- 00_warmup/warmup.py
+++ 00_warmup/warmup.py
@@ -65,156 +65,157 @@ import re
 import random
 import unittest
 import sys
+import argparse
 
 # Definicje wyjątków:
 
 class FilePathError(Exception):
-	"""Niepoprawna ścieżka do pliku."""
-	pass
+    """Niepoprawna ścieżka do pliku."""
+    pass
 
 
 class NotAFileError(Exception):
-	"""Ścieżka nie wskazuje na plik"""
-	pass
+    """Ścieżka nie wskazuje na plik"""
+    pass
 
 
 class NotEnoughCorrectWordsError(Exception):
-	"""Plik nie zawiera dostatecznej liczby poprawnych wyrazów."""
-	pass
+    """Plik nie zawiera dostatecznej liczby poprawnych wyrazów."""
+    pass
 
 
 class NotEnoughWordsError(Exception):
-	"""Plik nie zawiera dostatecznej liczby wyrazów."""
-	pass
+    """Plik nie zawiera dostatecznej liczby wyrazów."""
+    pass
 
 
 # funkcje realizujące zadanie
 
 def get_path():
-	"""Pobiera od użytkownika ścieżkę pliku tekstowego.
-	Łatwo można zmienić sposób podawania pliku.
-	Można podać ścieżkę do pliku przez parametr.
-	Wyświetla krótką pomoc jeśli jako parametr podać --help"""
-	if len(sys.argv)>1:
-		if sys.argv[1] == '--help':
-			print "Usage:\n\t%s [filename]" % sys.argv[0]
-			exit()
-		else:
-			filename = sys.argv[1]
-	else:
-		try:
-			# importuję dopiero tutaj, żeby wygodniej obsłużyć możliwe wyjątki ;)
-			# np. może nie być biblioteki Tk lub ekranu graficznego
-			from Tkinter import Tk
-			from tkFileDialog import askopenfilename
-			Tk().withdraw()
-			filename = askopenfilename()
-		except:
-			# fallback do trybu tekstowego
-			filename = raw_input("Podaj ścieżkę do pliku: ")
-	return filename
+    """Pobiera od użytkownika ścieżkę pliku tekstowego.
+    Łatwo można zmienić sposób podawania pliku.
+    Można podać ścieżkę do pliku przez parametr.
+    Wyświetla krótką pomoc jeśli jako parametr podać --help"""
+    if len(sys.argv)>1:
+        if sys.argv[1] == '--help':
+            print "Usage:\n\t%s [filename]" % sys.argv[0]
+            exit()
+        else:
+            filename = sys.argv[1]
+    else:
+        try:
+            # importuję dopiero tutaj, żeby wygodniej obsłużyć możliwe wyjątki ;)
+            # np. może nie być biblioteki Tk lub ekranu graficznego
+            from Tkinter import Tk
+            from tkFileDialog import askopenfilename
+            Tk().withdraw()
+            filename = askopenfilename()
+        except:
+            # fallback do trybu tekstowego
+            filename = raw_input("Podaj ścieżkę do pliku: ")
+    return filename
 
 def get_file_content(path):
-	"""Wczytuje plik i zwraca jego zawartość w postaci łańcucha"""
-	if os.path.exists(path):
-		if os.path.isfile(path):
-			return open(path).read()
-		else:
-			raise NotAFileError, "Podany zasób nie jest plikiem"
-	else:
-		raise FilePathError, "Nieprawidłowa ścieżka"
+    """Wczytuje plik i zwraca jego zawartość w postaci łańcucha"""
+    if os.path.exists(path):
+        if os.path.isfile(path):
+            return open(path).read()
+        else:
+            raise NotAFileError, "Podany zasób nie jest plikiem"
+    else:
+        raise FilePathError, "Nieprawidłowa ścieżka"
 
 def get_words(words, min_length=4, how_many=4):
-	"""Usuwa słowa krótsze niż czteroliterowe ORAZ "wyrazy" zawierające niepoprawne znaki.
-	Jako argument przyjmuje łańcuch i zwraca listę z oczekiwanymi wyrazami."""
-	# wykorzystuję wyrażenia regularne - lepiej się nie da :) przy okazji wykorzystuję locals(), by łatwo dostać się do potrzebnej zmiennej
-	pattern = re.compile(r"\b[a-zA-Z]{%(min_length)d,}\b" % locals())
-	result = re.findall(pattern, words)
-	if len(result) >= how_many:
-		return result
-	else:
-		raise NotEnoughCorrectWordsError, "Plik zawiera za mało poprawnych wyrazów (potrzeba co najmniej %(needed)d, otrzymano %(got)d)" % {'needed':how_many, 'got': len(result)}
+    """Usuwa słowa krótsze niż czteroliterowe ORAZ "wyrazy" zawierające niepoprawne znaki.
+    Jako argument przyjmuje łańcuch i zwraca listę z oczekiwanymi wyrazami."""
+    # wykorzystuję wyrażenia regularne - lepiej się nie da :) przy okazji wykorzystuję locals(), by łatwo dostać się do potrzebnej zmiennej
+    pattern = re.compile(r"\b[a-zA-Z]{%(min_length)d,}\b" % locals())
+    result = re.findall(pattern, words)
+    if len(result) >= how_many:
+        return result
+    else:
+        raise NotEnoughCorrectWordsError, "Plik zawiera za mało poprawnych wyrazów (potrzeba co najmniej %(needed)d, otrzymano %(got)d)" % {'needed':how_many, 'got': len(result)}
 
 def get_random_words(wordlist, how_many=4):
-	"""Losuje kilka elementów z listy i zwraca jako listę."""
-	if len(wordlist)>=how_many:
-		return random.sample(wordlist,how_many)
-	else:
-		raise NotEnoughWordsError, "Za mało wyrazów. Potrzeba co najmniej %(needed)d, otrzymano %(got)d." % {'needed': how_many, 'got': len(wordlist)}
-		# ten wyjątek normalnie nie powinien nigdy zostać zgłoszony w trakcie działania programu
-		# powód jest prosty: zbyt mała liczba wyrazów zostanie wcześniej wyłapana w funkcji get_words :)
+    """Losuje kilka elementów z listy i zwraca jako listę."""
+    if len(wordlist)>=how_many:
+        return random.sample(wordlist,how_many)
+    else:
+        raise NotEnoughWordsError, "Za mało wyrazów. Potrzeba co najmniej %(needed)d, otrzymano %(got)d." % {'needed': how_many, 'got': len(wordlist)}
+        # ten wyjątek normalnie nie powinien nigdy zostać zgłoszony w trakcie działania programu
+        # powód jest prosty: zbyt mała liczba wyrazów zostanie wcześniej wyłapana w funkcji get_words :)
 
 def find_intersections(wordlist):
-	"""Znajduje część wspólną w wyrazach podanych poprzez listę i zwraca jako listę."""
-	data = [set(word.lower()) for word in wordlist] # korzystam z listy składanej - zamieniam każdy wyraz na małe litery, przerabiam na zbiór i każdy ze zbiorów wrzucam jako element listy
-	return list(set.intersection(*data)) # zwraca listę elementów należących do części wspólnej zbiorów rozpakowanych z listy :)
+    """Znajduje część wspólną w wyrazach podanych poprzez listę i zwraca jako listę."""
+    data = [set(word.lower()) for word in wordlist] # korzystam z listy składanej - zamieniam każdy wyraz na małe litery, przerabiam na zbiór i każdy ze zbiorów wrzucam jako element listy
+    return list(set.intersection(*data)) # zwraca listę elementów należących do części wspólnej zbiorów rozpakowanych z listy :)
 
 def do_your_business():
-	path = get_path()
-	content = get_file_content(path)
-	words = get_words(content)
-	randwords = get_random_words(words)
-	result = find_intersections(randwords)
+    path = get_path()
+    content = get_file_content(path)
+    words = get_words(content)
+    randwords = get_random_words(words)
+    result = find_intersections(randwords)
 
-	print "*** Oto wylosowane wyrazy: "
-	for word in randwords:
-		print(word)
-	if len(result)>0:
-		print "*** Część wspólna: "
-		for letter in result:
-			print(letter)
-	else:
-		print "Wyrazy nie mają części wspólnej"
+    print "*** Oto wylosowane wyrazy: "
+    for word in randwords:
+        print(word)
+    if len(result)>0:
+        print "*** Część wspólna: "
+        for letter in result:
+            print(letter)
+    else:
+        print "Wyrazy nie mają części wspólnej"
 
 def main():
-	try:
-		do_your_business()
-	except IOError:
-		print "Błąd otwarcia pliku"
-	except Exception, e:
-		print e
+    try:
+        do_your_business()
+    except IOError:
+        print "Błąd otwarcia pliku"
+    except Exception, e:
+        print e
 
 # HERE BE DRAGONS
 
 class TestWarmup(unittest.TestCase):
-	"""Klasa zawiera wyłącznie testy funkcji nie związanych z IO."""
-	def test_get_words(self):
-		self.assertRaises(TypeError, get_words)
+    """Klasa zawiera wyłącznie testy funkcji nie związanych z IO."""
+    def test_get_words(self):
+        self.assertRaises(TypeError, get_words)
 
-		test_input = "1 ala Ala ma kot h1"
-		expected_result = []
-		self.assertListEqual(get_words(test_input, min_length=4, how_many=0), expected_result)
+        test_input = "1 ala Ala ma kot h1"
+        expected_result = []
+        self.assertListEqual(get_words(test_input, min_length=4, how_many=0), expected_result)
 
-		test_input = "NIkto 1 ala Aleksandra ma kota h1123123 234234"
-		expected_result = ['NIkto', 'Aleksandra', 'kota']
-		self.assertListEqual(get_words(test_input, min_length=4, how_many=0), expected_result)
+        test_input = "NIkto 1 ala Aleksandra ma kota h1123123 234234"
+        expected_result = ['NIkto', 'Aleksandra', 'kota']
+        self.assertListEqual(get_words(test_input, min_length=4, how_many=0), expected_result)
 
-		test_input = "1 ala Aleksandra ma kota h1"
-		self.assertRaises(NotEnoughCorrectWordsError, get_words, words=test_input,min_length=4, how_many=4)
+        test_input = "1 ala Aleksandra ma kota h1"
+        self.assertRaises(NotEnoughCorrectWordsError, get_words, words=test_input,min_length=4, how_many=4)
 
-	def test_get_random_words(self):
-		self.assertRaises(TypeError, get_random_words)
+    def test_get_random_words(self):
+        self.assertRaises(TypeError, get_random_words)
 
-		test_input = ["Ala", 'ma', 'fajnego', 'kota']
-		self.assertListEqual(sorted(get_random_words(test_input, how_many=4)), sorted(test_input))
-		# sortowanie obu list jest konieczne ze względu na losową kolejność elementów w wyniku
-		# zamiast tego można by ewentualnie zamienić listy na zbiory i porównać
+        test_input = ["Ala", 'ma', 'fajnego', 'kota']
+        self.assertListEqual(sorted(get_random_words(test_input, how_many=4)), sorted(test_input))
+        # sortowanie obu list jest konieczne ze względu na losową kolejność elementów w wyniku
+        # zamiast tego można by ewentualnie zamienić listy na zbiory i porównać
 
-		test_input = ["Ala", 'ma', 'kota']
-		self.assertRaises(NotEnoughWordsError, get_random_words, wordlist=test_input, how_many=4)
+        test_input = ["Ala", 'ma', 'kota']
+        self.assertRaises(NotEnoughWordsError, get_random_words, wordlist=test_input, how_many=4)
 
-	def test_find_intersections(self):
-		self.assertRaises(TypeError, find_intersections)
+    def test_find_intersections(self):
+        self.assertRaises(TypeError, find_intersections)
 
-		test_input = ["Ala", "Agi"]
-		expected_result = ['a']
-		self.assertListEqual(find_intersections(test_input), expected_result)
+        test_input = ["Ala", "Agi"]
+        expected_result = ['a']
+        self.assertListEqual(find_intersections(test_input), expected_result)
 
-		test_input = ["Jurek", "Ala", "Ola"]
-		expected_result = []
-		self.assertListEqual(find_intersections(test_input), expected_result)
+        test_input = ["Jurek", "Ala", "Ola"]
+        expected_result = []
+        self.assertListEqual(find_intersections(test_input), expected_result)
 
 # Beam me up, Scotty
 
 if __name__ == '__main__':
-	main()
+    main()
